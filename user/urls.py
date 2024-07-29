40983896806d32a08eda87d9fd9c68c323fe712f
Jose Arroyave <josearroyave@whaleandjaguar.com>
2024-07-29 15:15:05 -0500

    feat: add api methods for create users and add permissions

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,19 @@
 from django.urls import path, include
+
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from .views import UserViewSet, PermissionViewSet
+
+user_router = DefaultRouter()
+user_router.register(prefix=r'users', viewset=UserViewSet, basename='user')
+
+permission_router = DefaultRouter()
+permission_router.register(prefix=r'permissions', viewset=PermissionViewSet, basename='permission')
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
+    path('', include(user_router.urls)),
+    path('', include(permission_router.urls)),
+
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers.user_serializers import UserSerializer
+from .serializers.permission_serializer import PermissionSerializer
+
+from .services.user_services import create_user, add_permissions_to_user
+from .selectors.user_selectors import get_all_users
+from .selectors.permission_selectors import get_all_permissions
+
+class UserViewSet(GenericViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+
+        new_user_data = request.data.copy()
+        new_user_serializer = self.get_serializer(data=new_user_data)
+
+        if not new_user_serializer.is_valid():
+            return Response(
+                data={"errors": new_user_serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        new_user = create_user(user_data=new_user_data)
+        
+        return Response(
+             data={
+                "user": self.get_serializer(new_user).data,
+             }, 
+            status=status.HTTP_200_OK
+        )
+    
+    def list(self, request, *args, **kwargs):
+        
+        return Response(
+            data={
+                "users": self.get_serializer(
+                    get_all_users(),
+                    many=True,
+                    ).data,
+            },
+            status=status.HTTP_200_OK
+        )
+
+    @action(methods=["PATCH"], detail=True)
+    def add_permissions(self, request, pk=None, *args, **kwargs):
+        body = request.data.copy()
+        
+        permission_ids = body.get("permission_ids", None)
+        
+        if not permission_ids:
+            return Response(
+                data={"error": "permission_ids field is required."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        add_permissions_to_user(user_id=pk, permission_ids=permission_ids)
+        return Response(
+            data={"message": "permissions added successfully!"},
+            status=status.HTTP_200_OK,
+        )
+    
+class PermissionViewSet(GenericViewSet):
+    serializer_class = PermissionSerializer
+    
+    def list(self, request, *args, **kwargs):
+        
+        permissions_serializer = self.get_serializer(get_all_permissions(), many=True)
+    
+        return Response(
+            data={
+                "permissions": permissions_serializer.data
+            }, 
+            status=status.HTTP_200_OK
+            )
+    
