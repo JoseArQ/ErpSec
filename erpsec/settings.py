4f786aec3498fd1fd8251bee66c21155f4fdb2e7
Jose Arroyave <josearroyave@whaleandjaguar.com>
2024-07-29 13:42:41 -0500

    feat: add user model and authentication method.

--- erpsec/settings.py
+++ erpsec/settings.py
@@ -41,9 +41,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    ## REST FRAMEWORK
+    'rest_framework',
+    'rest_framework.authtoken',
     ## LOCAL APPS
     'user',
 ]
+
+AUTH_USER_MODEL = "user.User"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- user/models.py
+++ user/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, first_name, last_name, **extra_fields):
+        return self._create_user(email, password, first_name, last_name, password, **extra_fields)
+    
+    def create_superuser(self, email, password, first_name, last_name, **extra_fields):
+        extra_fields.setdefault('is_superuser',True)
+        return self._create_user(email, password, first_name, last_name, **extra_fields)
+    
+    def _create_user(self, email, password, first_name, last_name, **extra_fields):
+        if not email:
+            raise ValueError("Email must be provided")
+        if not password:
+            raise ValueError('Password is not provided')
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            first_name = first_name,
+            last_name = last_name,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    email = models.EmailField(db_index=True, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    last_login = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    def get_username(self):
+        return f"{self.email}"
+    
+    class Meta:
+        db_table = 'user' 
+        ordering = ["-created_at"] 
+
